ef6f0c85d8d0f3514bce15650e0414cf8816adf2
root <root@LAPTOP-F5VFQF4J>
2024-07-23 19:22:28 -0400

    mlflow in local v3

--- deployment/scripts/mlflow/aws/create_mlflow_aws.sh
+++ deployment/scripts/mlflow/aws/create_mlflow_aws.sh
@@ -9,9 +9,20 @@ AWS_EC2_KEY_PAR="${AWS_EC2_KEY_PAR_NAME}.pem"
 MLFLOW_BACKEND_STORE_URI="postgresql://{AWS_RDS_DB_USER}:{AWS_RDS_DB_PASSWORD}@{AWS_RDS_DB_ENDPOINT}:${AWS_RDS_DB_PORT}/${AWS_RDS_DB_NAME}"
 MLFLOW_ARTIFACT_ROOT="s3://${AWS_S3_BUCKET_NAME}"
 
+# SSH
 ssh -i "${AWS_EC2_KEY_PAR}" ec2-user@$AWS_EC2_DNS_PUBLIC <<EOF
 
-echo "Conectado a la instancia EC2. Iniciando servidor MLflow..."
+# Commands to execute on the EC2 instance
+echo "Connected to the EC2 instance. Starting MLflow server..."
+
+sudo yum update
+sudo yum install python-pip -y
+pip3 install mlflow boto3 psycopg2-binary
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_REGION=$AWS_REGION
+aws s3 ls
+
 mlflow server \
   -h 0.0.0.0 \
   -p 5000 \
