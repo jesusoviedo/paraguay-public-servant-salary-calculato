cca80030477fa085155bda206d2ea0ff12ce0601
Jesus Oviedo Riquelme <j92riquelme@gmail.com>
2024-08-18 15:34:54 -0400

    ci/cd fix

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -11,9 +11,6 @@ resource "aws_ecr_repository" "rj92_aws_docker_repository" {
 
 
 data aws_ecr_image flask_image {
- depends_on = [
-   null_resource.ecr_image
- ]
  repository_name = var.ecr_repo_name
  image_tag       = var.ecr_image_tag
 }
