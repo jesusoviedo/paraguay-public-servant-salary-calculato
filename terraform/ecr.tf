74cc5bbe9ed555e54d389b91b8f4b0515bbb27fa
Jesus Oviedo Riquelme <j92riquelme@gmail.com>
2024-08-18 13:55:13 -0400

    ci/cd, terraform and utils script update

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -9,20 +9,20 @@ resource "aws_ecr_repository" "rj92_aws_docker_repository" {
   force_delete = true
 }
 
-resource null_resource ecr_image {
-   triggers = {
-     docker_file = md5(file(var.ecr_docker_image_flask_local_path))
-   }
+#resource null_resource ecr_image {
+#   triggers = {
+#     docker_file = md5(file(var.ecr_docker_image_flask_local_path))
+#   }
 
-   provisioner "local-exec" {
-     command = <<EOF
-             aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
-             cd ./file/flask
-             docker build -t ${aws_ecr_repository.rj92_aws_docker_repository.repository_url}:${var.ecr_image_tag} .
-             docker push ${aws_ecr_repository.rj92_aws_docker_repository.repository_url}:${var.ecr_image_tag}
-         EOF
-   }
-}
+#   provisioner "local-exec" {
+#     command = <<EOF
+#             aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
+#             cd ./file/flask
+#             docker build -t ${aws_ecr_repository.rj92_aws_docker_repository.repository_url}:${var.ecr_image_tag} .
+#             docker push ${aws_ecr_repository.rj92_aws_docker_repository.repository_url}:${var.ecr_image_tag}
+#         EOF
+#   }
+#}
 
 
 data aws_ecr_image flask_image {
--- terraform/vars/dev.tfvars
+++ terraform/vars/dev.tfvars
@@ -17,4 +17,4 @@ rds_db_name_mage                    = env("RDS_DB_NAME_MAGE_DEV")
 s3_bucket_name_mlflow               = env("S3_BUCKET_NAME_MLFLOW_DEV")
 
 # Setting up ECR
-ecr_docker_image_flask_local_path   = "./file/flask/Dockerfile"
+#ecr_docker_image_flask_local_path   = "./file/flask/Dockerfile"
